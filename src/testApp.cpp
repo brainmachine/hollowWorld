f811204fdf1f2046e7c44712f0366c08c53dbb01
brainmachine <leostefansson@gmail.com>
2014-01-06 02:57:43 +0000

    enable depth test, circular points (smooth), 2000 stars, cleaned out example code

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -8,12 +8,15 @@ void testApp::setup(){
 	// Load a CSV File.
 	csv.loadFile(ofToDataPath("hygxyzSmall.csv"));
     mesh.setMode(OF_PRIMITIVE_POINTS);
+    ofEnableDepthTest();
+	glEnable(GL_POINT_SMOOTH); // use circular points instead of square points
+	//glPointSize(3); // make the points bigger
+    
 	cout << "Maximum Size:";
 	cout << csv.data[0].max_size() << endl;
-    for (int i = 0; i < 198; i++) {
+    for (int i = 0; i < 2000; i++) {
         // X value
         ofLogNotice("X: "+ofToString(csv.data[i][17]+" Y: "+ofToString(csv.data[i][18])+" Z: "+ofToString(csv.data[i][19])));
-        //
         //
         float x = csv.getFloat(i, 17);
         float y = csv.getFloat(i, 18);
@@ -32,8 +35,6 @@ void testApp::update(){
 //--------------------------------------------------------------
 void testApp::draw(){
     cam.begin();
-	//ofScale(2, -2, 2); // flip the y axis and zoom in a bit
-	//ofRotateY(90);
 	mesh.draw();
 	cam.end();
 }
@@ -41,38 +42,11 @@ void testApp::draw(){
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
 	
-	if (key == 's') {
-		// Get a specific value as inegert, float, String etc.
-		csv.setInt(0, 0, 2305);
-		cout << "getInt: " << csv.getInt(0, 0) << endl;
-		csv.setFloat(0, 1, 23.666);
-		cout << "getFloat: " << csv.getFloat(0, 1) << endl;
-		csv.setString(0, 2, "helloworld");
-		cout << "getString: " << csv.getString(0, 2) << endl;
-		csv.setBool(0, 3, true);
-		cout << "getBool: " << csv.getBool(0, 3) << endl;
-		
-		// Save File.
-		csv.saveFile(ofToDataPath("savefile.csv"));
-	}
-	else if (key == 'x') {
-		// clear all data from csvRecorder
-		csvRecorder.clear();
-	}
-	else if (key == 'r') {
-		// Save the recorded values in the csvRecorder ofxCsv object
-		csvRecorder.saveFile( ofToDataPath( "MyRecordedMouseData.csv" ));
-		ofLog() << "Saved " << csvRecorder.numRows << " rows of mouse data";
-	}
-	
 }
 
 //--------------------------------------------------------------
 void testApp::keyReleased(int key){
-	
-	// Create a new File.
-	csv.createFile(ofToDataPath("createfile.csv"));
-	
+
 }
 
 //--------------------------------------------------------------
@@ -82,26 +56,17 @@ void testApp::mouseMoved(int x, int y ){
 
 //--------------------------------------------------------------
 void testApp::mouseDragged(int x, int y, int button){
-	
-	// Add the current cursor position to the mouseData
-	int row = csvRecorder.numRows;
-	csvRecorder.setInt(row, 0, x);
-	csvRecorder.setInt(row, 1, y);
-	
+
 }
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button){
 	
-	// change the recording state for the mouse
-	recordingMouse = true;
 }
 
 //--------------------------------------------------------------
 void testApp::mouseReleased(int x, int y, int button){
 	
-	// change the recording state for the mouse
-	recordingMouse = false;
 }
 
 //--------------------------------------------------------------
